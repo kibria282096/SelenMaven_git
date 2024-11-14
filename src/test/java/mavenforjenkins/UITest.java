23ef6818ac857deb0acfb319dbce0dfdb44bcb0e
kibria282096 <jucse282096@gmail.com>
2024-11-14 19:27:57 +0600

    learning jenkins

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -8,33 +8,36 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class UITest 
-{
+public class UITest {
 
-	@Parameters("Browser")
-	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
-		}
-		else if(browserName.contains("Edge"))
-		{
-			 driver=new EdgeDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
-		driver.quit();
-	}
-	
-	
+    WebDriver driver; // Declare WebDriver at the class level
+
+    @Parameters("Browser")
+    @Test
+    public void startBrowser(String browserName) {
+        System.out.println("Parameter value is " + browserName);
+
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aristo Computers\\Downloads\\chromedriver-win64\\chromedriver.exe");
+ // Update with your ChromeDriver path
+            ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("--headless");
+            opt.addArguments("--no-sandbox");
+            opt.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(opt);
+        } else if (browserName.equalsIgnoreCase("Edge")) {
+            System.setProperty("webdriver.edge.driver", "path/to/edgedriver"); // Update with your EdgeDriver path
+            driver = new EdgeDriver();
+        }
+
+        // Ensure driver is initialized before calling manage() method
+        if (driver != null) {
+            driver.manage().window().maximize();
+            driver.get("https://opensource-demo.orangehrmlive.com/");
+            Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+            driver.quit();
+        } else {
+            System.out.println("Browser not recognized or not initialized");
+        }
+    }
 }
